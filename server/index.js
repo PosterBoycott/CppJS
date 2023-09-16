20ab5ebe8d780d9c51ed65dbd30b88b4740e70c7
Thinus De Beer <thinus5422@gmail.com>
2023-09-16 08:29:26 +0200

    Added routes

--- server/index.js
+++ server/index.js
@@ -3,11 +3,15 @@ import mongoose from 'mongoose'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 
+import postRoutes from './routes/posts.js'
+
 const app = express()
 
 app.use(bodyParser.json({limit:"30mb", extende: true}))
 app.use(bodyParser.urlencoded({limit:"30mb", extende: true}))
 app.use(cors())
+
+app.use('/posts', postRoutes)
 
 const PORT = process.env.PORT || 3000
 const CONNECTION_URL = 'mongodb+srv://thinus1234:thinus1234@posterboy.dzzceor.mongodb.net/'
